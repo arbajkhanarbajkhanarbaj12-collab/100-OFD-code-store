c597244ebed083b09107cff6c8b8332aefc8c1f9
arbajkhanarbajkhanarbaj12-collab <arbajkhanarbajkhanarbaj12@gmail.com>
2026-03-08 18:02:15 +0530

    Update start.py

--- handlers/start.py
+++ handlers/start.py
@@ -1,1 +1,9 @@
+from utils.keyboards import main_menu
 
+def start(bot, message):
+
+    bot.send_message(
+        message.chat.id,
+        "Welcome to Telegram Coupon Shop",
+        reply_markup=main_menu()
+    )
