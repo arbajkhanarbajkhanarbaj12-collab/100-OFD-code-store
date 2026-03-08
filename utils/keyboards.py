3c2b5a39bafcaca93760cc91645a688d314eeba2
arbajkhanarbajkhanarbaj12-collab <arbajkhanarbajkhanarbaj12@gmail.com>
2026-03-08 18:01:48 +0530

    Update keyboards.py

--- utils/keyboards.py
+++ utils/keyboards.py
@@ -1,1 +1,19 @@
+from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton
 
+def main_menu():
+
+    markup = InlineKeyboardMarkup()
+
+    markup.add(
+        InlineKeyboardButton("🛒 Shop", callback_data="shop")
+    )
+
+    markup.add(
+        InlineKeyboardButton("🎁 Redeem Gift", callback_data="redeem")
+    )
+
+    markup.add(
+        InlineKeyboardButton("💰 Wallet", callback_data="wallet")
+    )
+
+    return markup
