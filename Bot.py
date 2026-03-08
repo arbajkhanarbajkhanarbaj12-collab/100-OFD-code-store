0c5343e66e15aa67c981ba10cb2087d1fd722fdb
arbajkhanarbajkhanarbaj12-collab <arbajkhanarbajkhanarbaj12@gmail.com>
2026-03-08 18:03:22 +0530

    Update Bot.py

--- Bot.py
+++ Bot.py
@@ -1,1 +1,13 @@
+import telebot
+from config import BOT_TOKEN
+from handlers.start import start
 
+bot = telebot.TeleBot(BOT_TOKEN)
+
+@bot.message_handler(commands=['start'])
+
+def start_command(message):
+
+    start(bot, message)
+
+bot.infinity_polling()
